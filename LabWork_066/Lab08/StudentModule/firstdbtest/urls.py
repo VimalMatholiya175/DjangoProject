3ef6b7723d1f3f01bd59fe340d32099caa6e6951
VimalMatholiya175 <vimalmatholiya175@gmail.com>
2021-02-24 22:11:10 +0530

    Commited

---
+++ LabWork_066/Lab08/StudentModule/firstdbtest/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+		url(r'^addstudentinfo/$', views.addstudentinfo),
+		url(r'^getstudentinfo/$', views.getstudentinfo),
+		url(r'^addsuccess/$', views.addsuccess),
+		url('students/', views.StudentListView.as_view(), name ='students'),
+
+]
